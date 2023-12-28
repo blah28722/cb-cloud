3f1a8e287097e3b50c118beb776cb3173308dfac
John Chen <john.hk.chen@gmail.com>
2023-12-27 19:25:03 -0800

    Adopt new routing setup

--- api_xkcd/src/lib.rs
+++ api_xkcd/src/lib.rs
@@ -1,7 +1,27 @@
-use spin_sdk::http::{IntoResponse, Request, Method,Response};
+use anyhow::Result;
+use spin_sdk::http::{Request, Method, Response};
 use spin_sdk::http_component;
 use serde::Deserialize;
 use maud::html;
+
+
+#[http_component]
+async fn route_xkcd_requests(req: Request) -> Result<Response> {
+    match req.method() {
+        Method::Get => handle_api_xkcd(req).await,
+        Method::Options => preflight_cors_allow(req),
+        _ => panic!("Unexpected Request")
+    }
+}
+
+fn preflight_cors_allow(_req: Request) -> Result<Response> {
+    Ok(Response::builder()
+        .status(200)
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "GET, OPTIONS")
+        .header("Access-Control-Allow-Headers", "*")
+        .build())
+}
 
 #[derive(Deserialize, Debug)]
 struct ComicData {
@@ -9,9 +29,7 @@ struct ComicData {
     safe_title: String,
     alt: String,
 }
-
-#[http_component]
-async fn handle_api_xkcd(_req: Request) -> anyhow::Result<impl IntoResponse> {
+async fn handle_api_xkcd(_req: Request) -> Result<Response> {
     // Create the outbound request object
     let req = Request::builder()
         .method(Method::Get)
@@ -27,6 +45,7 @@ async fn handle_api_xkcd(_req: Request) -> anyhow::Result<impl IntoResponse> {
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/html")
+        .header("Access-Control-Allow-Origin", "*")
         .body(body)
         .build())
 }
--- htmx-sandbox/src/lib.rs
+++ htmx-sandbox/src/lib.rs
@@ -1,13 +1,109 @@
-use spin_sdk::http::{IntoResponse, Request, Response};
+use anyhow::Result;
+use maud::{html, PreEscaped};
+use spin_sdk::http::{Params, Request, Response, Router};
 use spin_sdk::http_component;
 
-/// A simple Spin HTTP component.
+fn root_uri() -> String {
+    let local_dev = false;
+
+    match local_dev {
+        true => String::from(""),
+        false => String::from("https://cb-cloud-ksksxilp.fermyon.app")
+    }
+}
+
+macro_rules! register_routes {
+    ($router:expr, $preflight_handler:expr, $( $path:expr => $handler:expr ),*) => {
+        $(
+            $router.get($path, $handler);
+            $router.options($path, $preflight_handler);
+        )*
+    }
+}
+
 #[http_component]
-fn handle_htmx_sandbox(req: Request) -> anyhow::Result<impl IntoResponse> {
-    println!("Handling request to {:?}", req.header("spin-full-url"));
+fn handle_htmx_sandbox(req: Request) -> Response {
+    let mut router = Router::new();
+    register_routes!(
+        router,
+        preflight_cors_allow,
+        "/rest/" => root_button,
+        "/rest/0" => step_zero,
+        "/rest/1" => step_one,
+        "/rest/2" => step_two
+    );
+    router.handle(req)
+}
+
+fn preflight_cors_allow(_req: Request, _params: Params) -> Result<Response> {
     Ok(Response::builder()
         .status(200)
-        .header("content-type", "text/plain")
-        .body("Hello, HTMX")
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "GET, OPTIONS")
+        .header("Access-Control-Allow-Headers", "*")
         .build())
+}
+
+fn xs_res(body: PreEscaped<String>) -> Response {
+    // Cross site response headers
+    Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .header("Access-Control-Allow-Origin", "*")
+        .body(body.into_string())
+        .build()
+}
+
+fn root_button(_req: Request, _params: Params) -> Result<Response> {
+    let htmx_uri = "https://unpkg.com/htmx.org@1.9.10";
+    let sha = "sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC";
+    Ok(xs_res(html! {
+        head {
+            title {"My HTMX Sandbox"}
+            script src=(htmx_uri) integrity=(sha) crossorigin="anonymous" {}
+        }
+        body {
+            button hx-get="/rest/1" hx-swap="outerHTML" {
+                "I have a favor I need to ask you..."
+            }
+        }
+    }))
+}
+
+fn step_zero(_req: Request, _params: Params) -> Result<Response> {
+    let next_step = root_uri() + "/rest/1";
+    Ok(xs_res(html! {
+        body {
+            button hx-get=(next_step) hx-swap="outerHTML" {
+                "I have a favor I need to ask you..."
+            }
+        }
+    }))
+}
+fn step_one(_req: Request, _params: Params) -> Result<Response> {
+    let next_step = root_uri() + "/rest/2";
+    Ok(xs_res(html! {
+        p{
+            "You Found a Secret!"
+        }
+        button hx-get=(next_step) hx-swap="outerHTML" {
+            "Claim Your Reward!"
+        }
+    }))
+}
+
+fn step_two(_req: Request, _params: Params) -> Result<Response> {
+    Ok(xs_res(html! {
+        strong {
+            p {
+                "Wow! It's a..."
+                br;
+            }
+            a href="https://en.wikipedia.org/wiki/Pikachu" {
+                img
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp/256px-Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp.png"
+                    alt="It's a Pikachu!";
+            }
+        }
+    }))
 }
